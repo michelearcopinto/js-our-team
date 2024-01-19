8479b44e39cf17f512802715da9052fec213db63
michelearcopinto <guestphantom1@gmail.com>
2024-01-19 13:21:46 +0100

    Files and structure

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const names = ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
+
+const jobs = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
+
+const imgs = ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg", "walter-gordon-office-manager.jpg", "angela-lopez-social-media-manager.jpg", "scott-estrada-developer.jpg", "barbara-ramos-graphic-designer.jpg"];
+
+
